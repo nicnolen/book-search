8c60793f2aa900cb433cb5682e8dd44adf47848d
Nicholas Nolen <nicnolen@ymail.com>
2022-04-05 15:42:39 -0500

    console logged userData

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -47,7 +47,7 @@ const SavedBooks = () => {
   if (loading) {
     return <h2>LOADING...</h2>;
   }
-
+  console.log(userData);
   return (
     <>
       <Jumbotron fluid className="text-light bg-dark">
