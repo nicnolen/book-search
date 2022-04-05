7b5f177300883746c999542ead4427f20364aa8e
Nicholas Nolen <nicnolen@ymail.com>
2022-04-05 12:06:45 -0500

    defined Query resolvers in resolvers.js for finding logged in users

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,23 @@
+//TODO: RESOLVERS
+//! Import dependencies
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+//! Create resolver object
+const resolvers = {
+  //! Query resolvers
+  Query: {
+    //* Find logged in user
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({})
+          .select('-_v -password')
+          .populate('books');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+  },
