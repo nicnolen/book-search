a6555427e9a0b3f887c2f324b551a1a9c44bec7f
Nicholas Nolen <nicnolen@ymail.com>
2022-04-05 13:13:31 -0500

    installed and set up Apollo Client

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloProvider, //? Provides data to all other componments
+  ApolloClient, //? Initialize connection to GraphQL API server
+  InMemoryCache, //? Enables Apollo Client instances to cache API response data
+  createHttpLink, //? Control how Apollo Client makes a request
+} from '@apollo/client';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//! Establish new link to the GraphQL server at its /graphql api endpoint
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+//! Create connection to GraphQL API endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(), //* Created a new cache object
+});
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
