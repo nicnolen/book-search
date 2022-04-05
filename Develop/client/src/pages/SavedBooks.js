6813d131525ad23c23ca4c2f7e6b82f5abed589e
Nicholas Nolen <nicnolen@ymail.com>
2022-04-05 16:00:40 -0500

    defaulted to an empty array in the savedBooks field if there is no data

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -15,7 +15,7 @@ import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const userData = data?.me || [];
+  const userData = data?.me || { savedBooks: [] };
 
   const [removeBook] = useMutation(REMOVE_BOOK);
 
