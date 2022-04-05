8152e195a146ddc7e47b1a429a1b50306ff75c1c
Nicholas Nolen <nicnolen@ymail.com>
2022-04-05 11:56:21 -0500

    implemented Apollo Server in server.js and created initial graphQL schema is the schemas folder

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,12 +1,38 @@
+//TODO: MAIN SERVER
+//! Import Dependencies
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express'); // Import ApolloServer
+
+const { typeDefs, resolvers } = require('./schemas'); 
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+//! Start a new Apollo server
+const startServer = async () => {
+  //* create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  //* start the Apollo server
+  await server.start();
+
+  //* integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  //* log where we can go to test our GQL API
+  console.info(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+//! Initialize the Apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
