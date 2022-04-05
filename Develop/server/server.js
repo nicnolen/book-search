7a5927db0124775e2d663e25ccf143dcfe6bb6cf
Nicholas Nolen <nicnolen@ymail.com>
2022-04-05 12:15:17 -0500

    connected Apollo Server with Express.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -4,19 +4,20 @@ const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express'); // Import ApolloServer
 
-const { typeDefs, resolvers } = require('./schemas'); 
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//! Start a new Apollo server
-const startServer = async () => {
+//! Start the Apollo server
+const startServer = async (typeDefs, resolvers) => {
   //* create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    // context: authMiddleware, // inside utils/auth.js
   });
 
   //* start the Apollo server
@@ -25,23 +26,32 @@ const startServer = async () => {
   //* integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 
-  //* log where we can go to test our GQL API
+  //* log where to go to test GQL API
   console.info(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  //! Import Mongoose connections the first time the connection is opened
+  db.once('open', () => {
+    //* Start the server on successful connection
+    app.listen(PORT, () =>
+      console.info(`🌍 Now listening on localhost:${PORT}`)
+    );
+  });
 };
 
-//! Initialize the Apollo server
-startServer();
-
-app.use(express.urlencoded({ extended: false }));
+//! Set URL encoded data
+app.use(express.urlencoded({ extended: true })); //? If true, data is parsed with qs library which allows nested objects from query string. If false, data is parsed with querystring library which does not support nested objects from query strings
+//! Every request goes through this middleware and gets converted to JSON
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+//! If we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+//! Catch-all route where any route that isnt defined is treated as a 404 error
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client'));
 });
+
+// Initialize the Apollo server
+startServer(typeDefs, resolvers);
